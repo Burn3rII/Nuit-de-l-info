7f0e51609d205648a06aa149647d0f4e6d926425
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-08 04:13:56 +0100

    Rename home

--- nuitinfo/quizz/urls.py
+++ nuitinfo/quizz/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "quizz"
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("questiondetail/<int:question_id>/", views.detail, name="detail"),
     path('results/<int:question_id>/<str:is_correct>/', views.results,
          name='results'),
