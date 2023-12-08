5f6553770a8d448bea5686800d6555e293376e43
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-08 04:26:33 +0100

    clear exemple en attente d'implémenter

--- nuitinfo/exemple/admin.py
+++ nuitinfo/exemple/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
-from .models import Task, Email, User, DiffusionList
 
-admin.site.register(Task)
-admin.site.register(Email)
-admin.site.register(User)
-admin.site.register(DiffusionList)
